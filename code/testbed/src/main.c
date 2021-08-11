78f46ce116b4fd22c783784f0ac3aaf119bb7051
Jacob <jcappi0199@gmail.com>
2021-08-11 17:35:54 -0600

    Stable: Completed Assertions

--- code/testbed/src/main.c
+++ code/testbed/src/main.c
@@ -1,10 +1,16 @@
 #include <core/logger.h>
+#include <core/asserts.h>
 
 int main(){
-    LOG_FATAL("YO %s %s", "testing", "working");
-    LOG_ERROR("YO %s %s", "testing", "working");
-    LOG_WARNING("YO %s %s", "testing", "working");
-    LOG_INFO("YO %s %s", "testing", "working");
-    LOG_DEBUG("YO %s %s", "testing", "working");
-    LOG_TRACE("YO %s %s", "testing", "working");
+    ERI_LOG_FATAL("YO %s %s", "testing", "working");
+    ERI_LOG_ERROR("YO %s %s", "testing", "working");
+    ERI_LOG_WARNING("YO %s %s", "testing", "working");
+    ERI_LOG_INFO("YO %s %s", "testing", "working");
+    ERI_LOG_DEBUG("YO %s %s", "testing", "working");
+    ERI_LOG_TRACE("YO %s %s", "testing", "working");
+
+    ERI_ASSERT( 10 == 1);
+    ERI_ASSERT_MESSAGE( 10 == 1, "Testing a message");
+    ERI_ASSERT_MESSAGE( 10 == 1, "Testing another message");
+    
 }
